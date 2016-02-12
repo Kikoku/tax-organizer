cec53adf78b177a3bad6526872ae2ef12fe04bcf
Collin <candrew022@gmail.com>
2016-02-12 17:56:59 -0500

    Created Organizer edit page

---
+++ src/pages/EditOrganizerPage.js
@@ -0,0 +1,14 @@
+import React, {Component, PropTypes} from 'react';
+import EditOrganizerContainer from '../containers/EditOrganizerContainer';
+
+class EditOrganizerPage extends Component {
+  render() {
+    return(
+      <div>
+        <EditOrganizerContainer />
+      </div>
+    )
+  }
+}
+
+export default EditOrganizerPage;
