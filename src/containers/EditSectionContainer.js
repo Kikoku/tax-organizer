c78462ff32992421ab23fc3232ef83704fd4ce07
Collin <candrew022@gmail.com>
2016-02-12 18:38:22 -0500

    Created Edit Sections Container

---
+++ src/containers/EditSectionContainer.js
@@ -0,0 +1,16 @@
+import React, {Component, PropTypes} from 'react';
+import EditQuestionContainer from './EditQuestionContainer'
+
+class EditSectionContainer extends Component {
+
+  render() {
+    return (
+      <div>
+        EditSectionContainer
+        <EditQuestionContainer />
+      </div>
+    );
+  }
+}
+
+export default EditSectionContainer;
