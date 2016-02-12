2ced91b60e09d066c3bc040c642b6024d5a37d58
Collin <candrew022@gmail.com>
2016-02-12 17:56:42 -0500

    Created Organizer Model
    Not sure if having questions at the root level will make it easier for
    responses

---
+++ api/models/Organizer.js
@@ -0,0 +1,50 @@
+import mongoose, {Schema} from 'mongoose';
+import Bluebird from 'bluebird';
+import timestamps from 'mongoose-timestamp';
+
+const organizerSchema = new Schema({
+  name: {
+    type: String,
+    default: ''
+  },
+  creator:  {
+    type: Schema.ObjectId,
+    ref: 'User'
+  },
+  sections: [{
+    name: String,
+    number: Number
+  }],
+  questions: [{
+    name: String,
+    number: Number,
+    type: String,
+    section: {
+      type: Schema.ObjectId
+    }
+  }],
+  options: [{
+    name: String,
+    number: Number,
+    question: {
+      type: Schema.ObjectId
+    }
+  }]
+
+});
+
+organizerSchema.methods = {
+
+}
+
+organizerSchema.plugin(timestamps, {
+  createdAt: 'created_at',
+  updatedAt: 'updated_at'
+});
+
+const Organizer = mongoose.model('Organizer', organizerSchema);
+
+Bluebird.promisifyAll(Organizer);
+Bluebird.promisifyAll(Organizer.prototype);
+
+export default Organizer;
