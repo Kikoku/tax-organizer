12091f3ffe18a9a4b2e70ca1fe4ceddbaa25fc62
Collin <candrew022@gmail.com>
2016-02-12 17:57:33 -0500

    Added edit to navigation

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,6 +5,7 @@ const Header = () => (
   <header className="Header row">
     <Link to="/signup" className="col-xs-12 col-md-2"><i className="fa fa-user-plus"></i> Signup</Link>
     <Link to="/login" className="col-xs-12 col-md-2"><i className="fa fa-lock"></i> Login</Link>
+    <Link to="/edit" className="col-xs-12 col-md-2"><i className="fa  fa-pencil-square-o"></i> Edit</Link>
   </header>
 );
 
