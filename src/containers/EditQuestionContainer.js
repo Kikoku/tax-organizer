280216fb8fb3e4dd5599412b4d205808c79b01aa
Collin <candrew022@gmail.com>
2016-02-12 18:38:45 -0500

    Created Edit Questions Container

---
+++ src/containers/EditQuestionContainer.js
@@ -0,0 +1,16 @@
+import React, {Component, PropTypes} from 'react';
+import EditOptionsContainer from './EditOptionsContainer';
+
+class EditQuestionContainer extends Component {
+
+  render() {
+    return (
+      <div>
+        EditQuestionContainer
+        <EditOptionsContainer />
+      </div>
+    );
+  }
+}
+
+export default EditQuestionContainer;
