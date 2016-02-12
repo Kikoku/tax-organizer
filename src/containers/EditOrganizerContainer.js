02e0ecec48c81fd7ef812cb4a3fb7ba0f295a4a6
Collin <candrew022@gmail.com>
2016-02-12 17:57:08 -0500

    Created Organizer Edit Container

---
+++ src/containers/EditOrganizerContainer.js
@@ -0,0 +1,15 @@
+import React, {Component, PropTypes} from 'react';
+import {connect} from 'react-redux';
+
+class EditOrganizerContainer extends Component {
+
+  render() {
+    return (
+      <div>
+        EditOrganizerContainer
+      </div>
+    );
+  }
+}
+
+export default EditOrganizerContainer;
