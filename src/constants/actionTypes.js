2d59af12f5596c54b9a620f267dabe7e2fb7636d
Collin <candrew022@gmail.com>
2016-02-11 22:47:08 -0500

    Added Login Types

--- src/constants/actionTypes.js
+++ src/constants/actionTypes.js
@@ -1,3 +1,7 @@
 export const SIGNUP = 'SIGNUP';
 export const SIGNUP_REQUEST = 'SIGNUP_REQUEST';
 export const SIGNUP_FAILURE = 'SIGNUP_FAILURE';
+
+export const LOGIN = 'LOGIN';
+export const LOGIN_REQUEST = 'LOGIN_REQUEST';
+export const LOGIN_FAILURE = 'LOGIN_FAILURE';
