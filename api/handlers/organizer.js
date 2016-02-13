44bcc7378a3adee3ffb1bd3f75863912350fa1d3
Collin <candrew022@gmail.com>
2016-02-13 10:39:48 -0500

    Added handlers for get routes

--- api/handlers/organizer.js
+++ api/handlers/organizer.js
@@ -56,5 +56,25 @@ export default {
 
     })
 
+  },
+  list: (request, reply) => {
+
+    Organizer.find()
+    .exec((err, organizers) => {
+
+      if(err) return reply(err);
+
+      reply(organizers);
+    })
+  },
+  show: (request, reply) => {
+
+    Organizer.findById(request.params.id)
+    .exec((err, organizer) => {
+
+      if(err) return reply(err);
+
+      reply(organizer);
+    })
   }
 }
