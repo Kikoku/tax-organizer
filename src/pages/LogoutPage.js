fda153ecd0aa96a43482aab2d927d6faa8c4e84a
Collin <candrew022@gmail.com>
2016-02-16 23:49:01 -0500

    Created Logout Page

---
+++ src/pages/LogoutPage.js
@@ -0,0 +1,14 @@
+import React, {Component, PropTypes} from 'react';
+import LogoutContainer from '../containers/LogoutContainer';
+
+class LogoutPage extends Component {
+  render() {
+    return(
+      <div>
+        <LogoutContainer />
+      </div>
+    )
+  }
+}
+
+export default LogoutPage;
