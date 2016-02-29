6c8d6b77845b10e4a44f12a7e90fe720cc4e27a1
Collin <candrew022@gmail.com>
2016-02-29 17:33:16 -0500

    Added a delete button to an OrganizerCard

--- src/components/OrganizerCard.js
+++ src/components/OrganizerCard.js
@@ -4,13 +4,18 @@ import {Link} from 'react-router';
 
 class OrganizerCard extends Component {
 
+  handleDeleteOrganizer = () => {
+    this.props.handleDeleteOrganizer(this.props.id);
+  }
+
   render() {
 
     const {name, id} = this.props;
 
     return(
       <div className="Container Question row">
-        <Link to={`/organizer/${id}`} className="col-lg-12">{name}</Link>
+        <Link to={`/organizer/${id}`} className="col-lg-11">{name}</Link>
+        <button onClick={this.handleDeleteOrganizer}><i className="fa fa-trash" /></button>
       </div>
     )
   }
