7033335b1612fc587cd7708e92cdca96d57eb182
Collin <candrew022@gmail.com>
2016-03-01 00:57:39 -0500

    Added delete to section container

--- src/containers/SectionContainer.js
+++ src/containers/SectionContainer.js
@@ -1,7 +1,20 @@
 import React, {Component, PropTypes} from 'react';
-import QuestionContainer from './QuestionContainer'
+import {connect} from 'react-redux';
+import QuestionContainer from './QuestionContainer';
+import Button from '../components/Button';
 
+import {deleteSection} from '../actions/OrganizerActions';
+
+@connect(state => ({
+  organizer: state.organizers.organizer
+}), {
+  deleteSection
+})
 class SectionContainer extends Component {
+
+  handleDelete = () => {
+    this.props.deleteSection(this.props.organizer._id, this.props._id);
+  }
 
   renderQuestion(input) {
     if(input) {
@@ -25,9 +38,14 @@ class SectionContainer extends Component {
 
     return (
       <div className="Container row">
-        <h2 className="col-lg-12">
+        <h2 className="col-xs-10 col-sm-10 col-md-10 col-lg-10">
           {name}
         </h2>
+        <Button className="Delete" onClick={this.handleDelete}>
+          <h2>
+            <i className="fa fa-trash" />
+          </h2>
+        </Button>
         {this.renderQuestion(questions)}
       </div>
     );
